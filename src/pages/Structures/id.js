90163f78771d63e5f68eb603abbdc1d4bb5f5c26
folland87 <frederic@olland.me>
2022-07-29 16:12:08 +0200

    add fetch functions

--- src/pages/Structures/id.js
+++ src/pages/Structures/id.js
@@ -1,12 +1,20 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Container, Title } from '@dataesr/react-dsfr';
-import useFetch from '../../hooks/useFetch';
+import fetch from '../../utils/fetch';
 
 export default function StructuresByIdPage() {
   const { id } = useParams();
-  const { data, error, isLoading } = useFetch('GET', `/structures/${id}`);
-  if (isLoading || !data) return <h1>Loading</h1>;
-  if (error) return <h1>Error</h1>;
+  const [data, setData] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch.get(`/structures/${id}`).catch((e) => { console.log(e); });
+      if (response.ok) setData(response.data);
+    };
+    getData();
+    return () => {};
+  }, [id]);
+  if (!data) return <h1>Loading</h1>;
   return (
     <Container spacing="mt-10w">
       <Title as="h1">Home</Title>
---
+++ src/utils/fetch.js
@@ -0,0 +1,74 @@
+const setDefaultHeaders = (requestHeaders = {}) => {
+  const accessToken = localStorage.getItem('__paysage_access__');
+  return {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: `Bearer ${accessToken}`,
+    ...requestHeaders,
+  };
+};
+
+const catchInvalidToken = async () => {
+  const refreshToken = localStorage.getItem('__paysage_refresh__');
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({ refreshToken }),
+    headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+  };
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/token`, options);
+  if (!response.ok) return null;
+  const tokens = await response.json();
+  const { accessToken: access, refreshToken: refresh } = tokens;
+  localStorage.setItem('__paysage_access__', access);
+  localStorage.setItem('__paysage_refresh__', refresh);
+  return access;
+};
+
+async function customFetch({
+  method, url, body, headers,
+}) {
+  const requestUrl = `${process.env.REACT_APP_API_URL}${url}`;
+  const options = {
+    method: method.toUpperCase(),
+    headers: setDefaultHeaders(headers),
+  };
+  if (body && ['POST', 'PUT', 'PATCH'].includes(method.toUpperCase())) { options.body = JSON.stringify(body); }
+  const response = await fetch(requestUrl, options);
+  if (response.ok) {
+    const json = await response.json();
+    response.data = json;
+    return response;
+  }
+  if (response.status === 401) {
+    const newAccessToken = await catchInvalidToken();
+    if (newAccessToken) {
+      options.headers.Authorization = `Bearer ${newAccessToken}`;
+      const retry = await fetch(requestUrl, options);
+      if (retry.ok) {
+        const rjson = await retry.json();
+        retry.data = rjson;
+        return retry;
+      }
+    }
+  }
+  throw new Error();
+}
+
+export default {
+  get: (url, headers) => customFetch({ method: 'GET', url, headers }),
+  post: (url, body, headers) => customFetch({
+    method: 'POST', url, body, headers,
+  }),
+  put: (url, body, headers) => customFetch({
+    method: 'PUT', url, body, headers,
+  }),
+  patch: (url, body, headers) => customFetch({
+    method: 'PATCH', url, body, headers,
+  }),
+  delete: (url, headers) => customFetch({ method: 'DELETE', url, headers }),
+  postFormData: (url, headers) => customFetch({
+    method: 'POST',
+    url,
+    headers: { ...headers, 'Content-Type': 'multipart/form-data' },
+  }),
+};
