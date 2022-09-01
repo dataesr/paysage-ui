bb519e618f743fa9e392f4b651d3c20a2639bcf6
Jerem <jeremy.peglion@gmail.com>
2022-09-01 11:37:12 +0200

    amélioration de la card pour qu'elle soit plus générique

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,21 +1,14 @@
-import { Icon } from '@dataesr/react-dsfr';
 import PropTypes from 'prop-types';
 import './styles.modules.scss';
 
-export default function Card({ title, description, buttonIcon, onClick }) {
+export default function Card({ title, description, element }) {
   return (
     <div className="fr-card fr-enlarge-link">
       <div className="fr-card__body">
         <div className="fr-card__content">
           {title ? <h4 className="fr-card__title">{title}</h4> : null}
           {description ? <p className="fr-card__desc">{description}</p> : null}
-          {
-            (buttonIcon) ? (
-              <button className="card-button" onClick={onClick} type="button">
-                <Icon className={buttonIcon} size="lg" />
-              </button>
-            ) : null
-          }
+          <div className="card-button">{element || null}</div>
         </div>
       </div>
     </div>
@@ -25,13 +18,11 @@ export default function Card({ title, description, buttonIcon, onClick }) {
 Card.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-  buttonIcon: PropTypes.string,
-  onClick: PropTypes.func,
+  element: PropTypes.node,
 };
 
 Card.defaultProps = {
   title: '',
   description: '',
-  buttonIcon: '',
-  onClick: {},
+  element: null,
 };
--- src/components/card/triple-dot-card.js
+++ src/components/card/triple-dot-card.js
@@ -1,13 +1,18 @@
+import { Icon } from '@dataesr/react-dsfr';
 import PropTypes from 'prop-types';
 import Card from './index';
 
 export default function TripleDotCard({ title, description, onClick }) {
+  const element = (
+    <button onClick={onClick} type="button">
+      <Icon className="ri-more-2-fill" size="lg" />
+    </button>
+  );
   return (
     <Card
       title={title}
       description={description}
-      onClick={onClick}
-      buttonIcon="ri-more-2-fill"
+      element={element}
     />
   );
 }
