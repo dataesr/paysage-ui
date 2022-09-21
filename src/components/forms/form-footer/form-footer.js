1d49df9195a7cf539b12b3b5e678fe37ce167b3f
Jerem <jeremy.peglion@gmail.com>
2022-09-21 11:58:05 +0200

    message de suppression en ligne

--- src/components/forms/form-footer/form-footer.js
+++ src/components/forms/form-footer/form-footer.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { ButtonGroup, Text } from '@dataesr/react-dsfr';
+import { ButtonGroup, Col, Row, Text } from '@dataesr/react-dsfr';
 import classNames from 'classnames';
 import Button from '../../button';
 import useViewport from '../../../hooks/useViewport';
@@ -13,39 +13,47 @@ export default function FormFooter({ id, onDeleteHandler, onSaveHandler }) {
     <>
       <hr />
       {confirm && (
-        <>
-          <Text bold>Etes-vous sur de vouloir supprimer cet élément ?</Text>
-          <ButtonGroup isInlineFrom="md">
-            <Button
-              onClick={() => setConfirm(false)}
-              secondary
-            >
-              Annuler
-            </Button>
-            <Button
-              onClick={() => onDeleteHandler(id)}
-              color="error"
-              icon="ri-chat-delete-line"
-            >
-              Supprimer
-            </Button>
-          </ButtonGroup>
-        </>
+        <Row>
+          <Col n="7">
+            <Text bold>Etes-vous sur de vouloir supprimer cet élément ?</Text>
+          </Col>
+          <Col>
+            <ButtonGroup isInlineFrom="md">
+              <Button
+                onClick={() => setConfirm(false)}
+                secondary
+              >
+                Annuler
+              </Button>
+              <Button
+                onClick={() => onDeleteHandler(id)}
+                color="error"
+                icon="ri-chat-delete-line"
+              >
+                Supprimer
+              </Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
       )}
       {!confirm && (
-        <ButtonGroup isInlineFrom="md" className={classnames}>
-          {(id) ? (
-            <Button
-              onClick={() => setConfirm(true)}
-              color="error"
-              secondary
-              icon="ri-chat-delete-line"
-            >
-              Supprimer
-            </Button>
-          ) : <div />}
-          <Button icon="ri-save-line" onClick={onSaveHandler}>Sauvegarder</Button>
-        </ButtonGroup>
+        <Row>
+          <Col>
+            <ButtonGroup isInlineFrom="md" className={classnames}>
+              {(id) ? (
+                <Button
+                  onClick={() => setConfirm(true)}
+                  color="error"
+                  secondary
+                  icon="ri-chat-delete-line"
+                >
+                  Supprimer
+                </Button>
+              ) : <div />}
+              <Button icon="ri-save-line" onClick={onSaveHandler}>Sauvegarder</Button>
+            </ButtonGroup>
+          </Col>
+        </Row>
       )}
     </>
   );
