9c8383c327270bffc96a35ed728ca925b01ef727
Anne L'Hôte <anne.lhote@gmail.com>
2022-10-03 18:06:12 +0200

    fix(structures): Rename "useBlocUrl" into "useUrl"

--- src/pages/structures/[id]/gouvernance.js
+++ src/pages/structures/[id]/gouvernance.js
@@ -1,12 +1,12 @@
 import useFetch from '../../../hooks/useFetch';
-import useBlocUrl from '../../../hooks/useBlocUrl';
+import useUrl from '../../../hooks/useUrl';
 import useHashScroll from '../../../hooks/useHashScroll';
 import Spinner from '../../../components/spinner';
 import Relations from '../../../components/blocs/relations';
 
 export default function StructureGouvernancePage() {
   useHashScroll();
-  const { url } = useBlocUrl();
+  const { url } = useUrl();
   const { data, isLoading, error } = useFetch(`${url}/relations-groups?filters[name][$in]=Gouvernance&filters[name][$in]=Référents MESR&limit=2`);
 
   if (isLoading) return <Spinner size={48} />;
