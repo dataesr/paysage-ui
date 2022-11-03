0ec764733b6d61cfbe4cf472d6b2fdf99658437a
Mihoub Debache <debache.mihoub@gmail.com>
2022-11-03 14:23:48 +0100

    feature(card) : add link with cardValue

--- src/components/card/key-value-card.js
+++ src/components/card/key-value-card.js
@@ -4,15 +4,44 @@ import useEditMode from '../../hooks/useEditMode';
 import Button from '../button';
 import CopyButton from '../copy/copy-button';
 
-export default function KeyValueCard({ cardKey, cardValue, icon, tooltip, onEdit, copy, className, titleAsText }) {
+export default function KeyValueCard({
+  cardKey,
+  cardValue,
+  className,
+  copy,
+  onEdit,
+  tooltip,
+  icon,
+  titleAsText,
+}) {
   const { editMode } = useEditMode();
+
+  let linkTo = '';
+  switch (cardKey) {
+  case 'Wikidata':
+    linkTo = `https://wikidata.org/wiki/${cardValue}`;
+    break;
+  case 'idRef':
+    linkTo = `https://www.idref.fr/${cardValue}`;
+    break;
+  case 'ORCID':
+    linkTo = `https://orcid.org/${cardValue}`;
+    break;
+  case 'ROR':
+    linkTo = `https://ror.org/${cardValue}`;
+    break;
+  default:
+  }
+
   return (
     <div className={`fr-card fr-card--xs fr-card--horizontal fr-card--grey fr-card--no-border ${className}`}>
       <div className="fr-card__body">
         <div className="fr-card__content">
           <p className="fr-card__title">
             <span
-              className={`fr-pr-1w ${(cardValue && titleAsText) && 'fr-text--md fr-text--regular'} ${!cardValue && 'fr-text--sm fr-text--regular italic'}`}
+              className={`fr-pr-1w ${
+                titleAsText && 'fr-text--md fr-text--regular'
+              } ${!cardValue && 'fr-text--sm fr-text--regular italic'}`}
             >
               {cardValue || 'Non renseigné'}
             </span>
@@ -21,11 +50,29 @@ export default function KeyValueCard({ cardKey, cardValue, icon, tooltip, onEdit
           <div className="fr-card__start">
             <p className="fr-card__detail fr-text--sm fr-mb-0">
               {icon && <Icon name={icon} size="1x" />}
-              {cardKey}
-              {tooltip && <Icon name="ri-information-fill" size="1x" iconPosition="right" title={tooltip} />}
+              <a href={linkTo} target="_blank" rel="noopener noreferrer">{cardKey}</a>
+              {tooltip && (
+                <Icon
+                  name="ri-information-fill"
+                  size="1x"
+                  iconPosition="right"
+                  title={tooltip}
+                />
+              )}
             </p>
           </div>
-          {(editMode && onEdit) && <Button color="text" size="md" onClick={onEdit} tertiary borderless rounded icon="ri-edit-line" className="edit-button" />}
+          {editMode && onEdit && (
+            <Button
+              color="text"
+              size="md"
+              onClick={onEdit}
+              tertiary
+              borderless
+              rounded
+              icon="ri-edit-line"
+              className="edit-button"
+            />
+          )}
         </div>
       </div>
     </div>
--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,4 +1,4 @@
 export function capitalize(str) {
   const string = `${str}`;
-  return string.slice(0, 1).toUpperCase() + string.slice(1).toLowerCase();
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
 }
