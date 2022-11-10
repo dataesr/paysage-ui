dc20a5d2b4c1a75d569cf734ef549a989dc51dce
Mihoub Debache <debache.mihoub@gmail.com>
2022-11-10 11:37:35 +0100

    fix(webLinks) add condition for jorfsearch links

--- src/components/blocs/weblinks/index.js
+++ src/components/blocs/weblinks/index.js
@@ -1,20 +1,26 @@
 import PropTypes from 'prop-types';
-import {
-  Icon,
-  Modal,
-  ModalContent,
-  ModalTitle,
-} from '@dataesr/react-dsfr';
+import { Icon, Modal, ModalContent, ModalTitle } from '@dataesr/react-dsfr';
 import { useState } from 'react';
 import ExpendableListCards from '../../card/expendable-list-cards';
 import api from '../../../utils/api';
 import WeblinkForm from '../../forms/weblinks';
-import { Bloc, BlocActionButton, BlocContent, BlocModal, BlocTitle } from '../../bloc';
+import {
+  Bloc,
+  BlocActionButton,
+  BlocContent,
+  BlocModal,
+  BlocTitle,
+} from '../../bloc';
 import useFetch from '../../../hooks/useFetch';
 import useUrl from '../../../hooks/useUrl';
 import useEnums from '../../../hooks/useEnums';
 import useNotice from '../../../hooks/useNotice';
-import { deleteError, saveError, saveSuccess, deleteSuccess } from '../../../utils/notice-contents';
+import {
+  deleteError,
+  saveError,
+  saveSuccess,
+  deleteSuccess,
+} from '../../../utils/notice-contents';
 import Button from '../../button';
 import useEditMode from '../../../hooks/useEditMode';
 
@@ -35,14 +41,21 @@ export default function Weblinks({ types, title }) {
     const method = body.id ? 'patch' : 'post';
     const saveUrl = body.id ? `${url}/${body.id}` : url;
     await api[method](saveUrl, body)
-      .then(() => { notice(saveSuccess); reload(); })
+      .then(() => {
+        notice(saveSuccess);
+        reload();
+      })
       .catch(() => notice(saveError));
     return setShowModal(false);
   };
 
   const onDeleteHandler = async (itemId) => {
-    await api.delete(`${url}/${itemId}`)
-      .then(() => { notice(deleteSuccess); reload(); })
+    await api
+      .delete(`${url}/${itemId}`)
+      .then(() => {
+        notice(deleteSuccess);
+        reload();
+      })
       .catch(() => notice(deleteError));
     return setShowModal(false);
   };
@@ -62,23 +75,30 @@ export default function Weblinks({ types, title }) {
 
   const getElementsCount = () => {
     if (!data) return null;
-    return types?.length ? data.data.filter((el) => types.includes(el.type))?.length : data.data.length;
+    return types?.length
+      ? data.data.filter((el) => types.includes(el.type))?.length
+      : data.data.length;
   };
 
   const renderIcon = (iconType) => {
     let rxIcon = '';
 
     switch (iconType) {
-    case 'Facebook':
-      rxIcon = 'ri-facebook-fill';
-      break;
-    default:
-      rxIcon = 'ri-global-line';
-      break;
+      case 'Facebook':
+        rxIcon = 'ri-facebook-fill';
+        break;
+      default:
+        rxIcon = 'ri-global-line';
+        break;
     }
 
     return (
-      <Icon className="fr-mb-1w fr-pt-1w" name={rxIcon} size="3x" color={`var(--${apiObject}-color)`} />
+      <Icon
+        className="fr-mb-1w fr-pt-1w"
+        name={rxIcon}
+        size="3x"
+        color={`var(--${apiObject}-color)`}
+      />
     );
   };
 
@@ -88,31 +108,77 @@ export default function Weblinks({ types, title }) {
       ? data.data.filter((el) => types.includes(el.type))
       : data.data;
     const list = filteredList.map((el) => (
-      <div key={el.id} className="fr-card fr-card--sm fr-card--grey fr-card--no-border">
+      <div
+        key={el.id}
+        className="fr-card fr-card--sm fr-card--grey fr-card--no-border"
+      >
         <div className={`fr-card__body ${!editMode && 'fr-enlarge-link'}`}>
           <div className="fr-card__content">
             <div className="flex-col flex--center">
               {renderIcon(el.type)}
               <span className="fr-text fr-text--sm fr-text--bold fr-m-0 flex-col">
-                <a className="fr-mb-0 fr-text fr-text--sm text-center" href={el.url} target="_blank" rel="noreferrer">
-                  {options?.find((type) => (el.type === type.value))?.label}
-                  {el.language && ` (${el.language})`}
-                </a>
+                {el.url.includes('jorfsearch.steinertriples') ? (
+                  <a
+                    className="fr-mb-0 fr-text fr-text--sm text-center"
+                    href={el.url.replace('.fr', '.ch')}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {options?.find((type) => el.type === type.value)?.label}
+                    {el.language && ` (${el.language})`}
+                  </a>
+                ) : (
+                  <a
+                    className="fr-mb-0 fr-text fr-text--sm text-center"
+                    href={el.url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {options?.find((type) => el.type === type.value)?.label}
+                    {el.language && ` (${el.language})`}
+                  </a>
+                )}
                 <span className="only-print">{el.url}</span>
               </span>
             </div>
-            {editMode && <Button color="text" size="md" onClick={() => onOpenModalHandler(el)} tertiary borderless rounded icon="ri-edit-line" className="edit-button" />}
+            {editMode && (
+              <Button
+                color="text"
+                size="md"
+                onClick={() => onOpenModalHandler(el)}
+                tertiary
+                borderless
+                rounded
+                icon="ri-edit-line"
+                className="edit-button"
+              />
+            )}
           </div>
         </div>
       </div>
     ));
-    return <ExpendableListCards list={list} nCol="12 lg-6 md-12 sm-6" order={types} sortOn="props.type" />;
+    return (
+      <ExpendableListCards
+        list={list}
+        nCol="12 lg-6 md-12 sm-6"
+        order={types}
+        sortOn="props.type"
+      />
+    );
   };
 
   return (
-    <Bloc isLoading={isLoading} error={error} data={{ totalCount: getElementsCount() }}>
-      <BlocTitle as="h4" look="h6">{title}</BlocTitle>
-      <BlocActionButton onClick={onOpenModalHandler} isSmall>Ajouter un lien</BlocActionButton>
+    <Bloc
+      isLoading={isLoading}
+      error={error}
+      data={{ totalCount: getElementsCount() }}
+    >
+      <BlocTitle as="h4" look="h6">
+        {title}
+      </BlocTitle>
+      <BlocActionButton onClick={onOpenModalHandler} isSmall>
+        Ajouter un lien
+      </BlocActionButton>
       <BlocContent>{renderCards()}</BlocContent>
       <BlocModal>
         <Modal isOpen={showModal} size="lg" hide={() => setShowModal(false)}>
