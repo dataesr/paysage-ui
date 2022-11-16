98f786401953de51cc411eb756c9f9c8b72c5fcc
folland87 <frederic@olland.me>
2022-11-16 09:49:55 +0100

    fix date print

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,26 +1,14 @@
-const dateOptions = {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-};
-const timeOptions = {
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric',
-};
-
-export function validDate(d) {
-  return /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/g.test(d);
-}
-
-export function fromApproximativeDate(d) {
-  if (!validDate(d)) return null;
-  if (d.length === 4) return `${d}-01-01`;
-  if (d.length === 7) return `${d}-01`;
-  return d;
-}
-
 export function toString(date, time = false, isCompact = false) {
+  const dateOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+  const timeOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
   let options = dateOptions;
   if (time) options = { ...options, ...timeOptions };
   if (isCompact) options.month = 'numeric';
@@ -53,17 +41,20 @@ export function formatDescriptionDates(startDate = null, endDate = null) {
     if (endDate.split('-').length === 1) {
       return `jusqu'à ${toString(endDate)}`;
     }
+    if (endDate.split('-').length === 2) {
+      return `jusqu'en ${toString(endDate)}`;
+    }
     return `jusqu'au ${toString(endDate)}`;
   }
   if (startDate && !endDate) {
-    if (startDate.split('-').length === 1) {
+    if (startDate.split('-').length !== 3) {
       return `depuis ${toString(startDate)}`;
     }
     return `depuis le ${toString(startDate)}`;
   }
   if (startDate && endDate) {
     let ret = '';
-    if (startDate.split('-').length === 1) {
+    if (startDate.split('-').length !== 3) {
       ret = `de ${toString(startDate)} `;
     } else {
       ret = `du ${toString(startDate)} `;
