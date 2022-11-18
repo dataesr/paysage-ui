7a91f6a7f0e14212dcda183fae3b14c85c5d8e83
Mihoub Debache <debache.mihoub@gmail.com>
2022-11-18 13:55:15 +0100

    fix(categories & termes): add descriptions field

--- src/pages/categories/[id]/presentation.js
+++ src/pages/categories/[id]/presentation.js
@@ -1,14 +1,25 @@
 import { Row, Title, Col } from '@dataesr/react-dsfr';
+
 import Identifiers from '../../../components/blocs/identifiers';
 import SocialMedias from '../../../components/blocs/social-medias';
 import Weblinks from '../../../components/blocs/weblinks';
 import RelationsByTag from '../../../components/blocs/relations-by-tag';
+import KeyValueCard from '../../../components/card/key-value-card';
+import Spinner from '../../../components/spinner';
+import useUrl from '../../../hooks/useUrl';
 import useHashScroll from '../../../hooks/useHashScroll';
+import useFetch from '../../../hooks/useFetch';
 import Wiki from '../../../components/blocs/wiki';
 import { CATEGORIE_PARENT } from '../../../utils/relations-tags';
 
 export default function CategoryPresentationPage() {
   useHashScroll();
+  const { url } = useUrl();
+  const { data, isLoading, error } = useFetch(url);
+
+  if (isLoading) return <Row className="fr-my-2w flex--space-around"><Spinner /></Row>;
+  if (error) return <>Erreur...</>;
+
   return (
     <>
       <RelationsByTag
@@ -28,6 +39,15 @@ export default function CategoryPresentationPage() {
         inverse
         sort="resource.priority"
       />
+      <Col n="12">
+        <KeyValueCard
+          titleAsText
+          className="card-categories"
+          cardKey="Description"
+          cardValue={data?.descriptionFr || data?.descriptionEn}
+          icon="ri-align-left"
+        />
+      </Col>
       <Title as="h3" look="h4">Présence sur le web</Title>
       <Row spacing="mb-5w"><Wiki /></Row>
       <Row gutters>
--- src/pages/termes/[id]/presentation.js
+++ src/pages/termes/[id]/presentation.js
@@ -2,6 +2,10 @@ import { Col, Row, Title } from '@dataesr/react-dsfr';
 import Identifiers from '../../../components/blocs/identifiers';
 import SocialMedias from '../../../components/blocs/social-medias';
 import Weblinks from '../../../components/blocs/weblinks';
+import Spinner from '../../../components/spinner';
+import useFetch from '../../../hooks/useFetch';
+import useUrl from '../../../hooks/useUrl';
+import KeyValueCard from '../../../components/card/key-value-card';
 import useHashScroll from '../../../hooks/useHashScroll';
 import Wiki from '../../../components/blocs/wiki';
 import RelationsByTag from '../../../components/blocs/relations-by-tag';
@@ -9,6 +13,12 @@ import { TERME_PARENT } from '../../../utils/relations-tags';
 
 export default function TermPresentationPage() {
   useHashScroll();
+  const { url } = useUrl();
+  const { data, isLoading, error } = useFetch(url);
+
+  if (isLoading) return <Row className="fr-my-2w flex--space-around"><Spinner /></Row>;
+  if (error) return <>Erreur...</>;
+
   return (
     <>
       <RelationsByTag
@@ -26,6 +36,15 @@ export default function TermPresentationPage() {
         noRelationType
         inverse
       />
+      <Col n="12">
+        <KeyValueCard
+          titleAsText
+          className="card-terms"
+          cardKey="Description"
+          cardValue={data?.descriptionFr || data?.descriptionEn}
+          icon="ri-align-left"
+        />
+      </Col>
       <Title as="h3" look="h4">Présence sur le web</Title>
       <Row spacing="mb-5w"><Wiki /></Row>
       <Row gutters>
