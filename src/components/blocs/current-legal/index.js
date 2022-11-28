ff335c494ff33cbd8d3f2a7874d41e6cac8b84ad
Mihoub Debache <debache.mihoub@gmail.com>
2022-11-28 14:38:44 +0100

    fix(current-legal): add keyValueCard

--- src/components/blocs/current-legal/index.js
+++ src/components/blocs/current-legal/index.js
@@ -4,6 +4,7 @@ import useFetch from '../../../hooks/useFetch';
 import useUrl from '../../../hooks/useUrl';
 import { getComparableNow } from '../../../utils/dates';
 import { STRUCTURE_CATEGORIE_JURIDIQUE as tag } from '../../../utils/relations-tags';
+import KeyValueCard from '../../card/key-value-card';
 
 export default function CurrentLegals() {
   const { id } = useUrl();
@@ -15,6 +16,13 @@ export default function CurrentLegals() {
     .filter((relation) => (!relation.endDate || (relation.endDate >= getComparableNow())))
     .sort((a, b) => a.startDate < b.startDate);
   const currentLegal = currentLegals?.[0] || {};
+  if (!currentLegal.relatedObject) {
+    return (
+      <Col n="12 lg-4">
+        <KeyValueCard className="card-legal-categories" cardKey="Catégorie juridique" icon="ri-spy-line" />
+      </Col>
+    );
+  }
   return (
     <Col n="12 lg-4">
       <div className="fr-card fr-card--xs fr-card--horizontal fr-card--grey fr-card--no-border card-legal-categories">
