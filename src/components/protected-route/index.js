8798c4ff072e3ffdccd9ce483de6e06f34e56632
folland87 <frederic@olland.me>
2022-11-29 14:51:58 +0100

    redirect to se-connecter if pathname is / and user is not connected

--- src/components/protected-route/index.js
+++ src/components/protected-route/index.js
@@ -1,14 +1,16 @@
 import PropTypes from 'prop-types';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import Error from '../errors';
 import { PageSpinner } from '../spinner';
 
 export default function ProtectedRoute({ roles }) {
   const { viewer, isLoading } = useAuth();
+  const { pathname } = useLocation();
   const canView = roles.length ? (viewer.id && roles.includes(viewer.role)) : !!viewer.id;
   if (isLoading) return <PageSpinner />;
   if (canView) return <Outlet />;
+  if (!canView && pathname === '/') return <Navigate to="/se-connecter" />;
   return <Error status="404" />;
 }
 
