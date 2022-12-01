2da15248893c5b2c9fc45890f0249df4acab3741
Mihoub Debache <debache.mihoub@gmail.com>
2022-12-01 11:40:34 +0100

    fix(not-found): clean style with module

--- src/components/layout/not-found.js
+++ src/components/layout/not-found.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, Text, Title } from '@dataesr/react-dsfr';
+import { Button, Text } from '@dataesr/react-dsfr';
 import { useNavigate } from 'react-router-dom';
-import './not-found.scss';
+import styles from './not-found.module.scss';
 
 const randomDisplay = Math.floor(Math.random() * 2);
 
@@ -10,54 +10,54 @@ export default function NotFound() {
 
   return randomDisplay === 1 ? (
     <div>
-      <div className="top-header" />
+      <div />
       <div>
-        <div className="starsec" />
-        <div className="starthird" />
-        <div className="starfourth" />
-        <div className="starfifth" />
+        <div className={styles.starsec} />
+        <div className={styles.starthird} />
+        <div className={styles.starfourth} />
+        <div className={styles.starfifth} />
       </div>
-      <div className="lamp__wrap">
-        <div className="lamp">
-          <div className="cable" />
-          <div className="cover" />
-          <div className="in-cover">
-            <div className="bulb" />
+      <div className={styles.lamp__wrap}>
+        <div className={styles.lamp}>
+          <div className={styles.cable} />
+          <div className={styles.cover} />
+          <div className={styles['in-cover']}>
+            <div className={styles.bulb} />
           </div>
-          <div className="light" />
+          <div className={styles.light} />
         </div>
       </div>
-      <div className="error">
-        <div className="error__content">
-          <div className="error__message message">
-            <Text className="message__title">Page Introuvable!</Text>
-            <Text className="message__text">Désolé, la page que vous cherchez n'existe pas. </Text>
-            <Text className="message__text"> Vous pouvez relancer une recherche ou vous rendre sur la page d'accueil</Text>
+      <div className={styles.error}>
+        <div className={styles.error__content}>
+          <div className={styles.error__message}>
+            <Text className={styles.message__title}>Page Introuvable!</Text>
+            <Text className={styles.message__title}>Désolé, la page que vous cherchez n'existe pas. </Text>
+            <Text className={styles.message__title}> Vous pouvez relancer une recherche ou vous rendre sur la page d'accueil</Text>
           </div>
           <Button
             onClick={() => navigate('/')}
           >
             En cliquant ici
           </Button>
-          <Text className="error__nav e-nav" />
+          <Text className={styles['error__nav e-nav']} />
         </div>
 
       </div>
 
     </div>
   ) : (
-    <div className="page_404">
-      <div className="container">
-        <div className="row">
+    <div className={styles.page_404}>
+      <div className={styles.container}>
+        <div className={styles.row}>
           <div className="col-sm-12 ">
             <div className="col-sm-10 col-sm-offset-1  text-center">
-              <div className="four_zero_four_bg">
-                <Title className="message__title">Page Introuvable!</Title>
+              <div className={styles.four_zero_four_bg}>
+                <Text className={styles.message__title}>Page Introuvable!</Text>
               </div>
               <Text>
-                <Text className="h2">
+                <Text className={styles.message__title}>
                   Désolé, la page que vous cherchez n'existe pas.
-                  <Text> Vous pouvez relancer une recherche ou vous rendre sur la page d'accueil</Text>
+                  <Text className={styles.message__title}> Vous pouvez relancer une recherche ou vous rendre sur la page d'accueil</Text>
                 </Text>
               </Text>
               <Button
