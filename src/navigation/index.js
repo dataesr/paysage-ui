0bee7d6551cf1e4c8bbff81198697c1e7d83d3da
folland87 <frederic@olland.me>
2022-12-19 12:48:20 +0100

    delete apikey page

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -76,8 +76,6 @@ import { PriceByIdPage, PriceCategories, PriceExportPage, PricePresentationPage 
 import SupervisingMinistersByIdPage from '../pages/supervising-ministers/id';
 import LegalCategoriesByIdPage from '../pages/legal-categories/id';
 
-import ApiKeysPage from '../pages/admin/api-keys';
-
 import ScrollToTopOnPathnameChange from './scroll-top-on-pathname-change';
 
 export default function Routes() {
@@ -96,7 +94,6 @@ export default function Routes() {
               <Route path="" element={<Navigate to="dashboard" replace />} />
               <Route path="dashboard" element={<AdminDashboardPage />} />
               <Route path="utilisateurs" element={<AdminUsersPage />} />
-              <Route path="cles-api" element={<ApiKeysPage />} />
               <Route path="groupes" element={<AdminGroupsPage />} />
               <Route path="categories-juridiques" element={<AdminLegalCategoriesPage />} />
               <Route path="types-de-relation" element={<AdminRelationTypesPage />} />
