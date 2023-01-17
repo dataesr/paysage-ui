bc5010c8d39c1f8d58f5a99d687ecc5642061012
Mihoub Debache <debache.mihoub@gmail.com>
2023-01-17 09:12:39 +0100

    fix(forms-relations): add active and inactive button in form

--- src/components/forms/relations/index.js
+++ src/components/forms/relations/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Container, Col, Row, SearchableSelect } from '@dataesr/react-dsfr';
+import { Container, Col, Checkbox, Row, SearchableSelect, RadioGroup, Radio } from '@dataesr/react-dsfr';
 import PropTypes from 'prop-types';
 import useForm from '../../../hooks/useForm';
 import DateInput from '../../date-input';
@@ -12,15 +12,17 @@ import PaysageBlame from '../../paysage-blame';
 
 function sanitize(form) {
   const fields = [
-    'resourceId',
+    'active',
+    'endDate',
+    'endDateOfficialTextId',
+    'inactive',
     'relatedObjectId',
-    'relationTypeId',
     'relationsGroupId',
     'relationTag',
-    'startDateOfficialTextId',
-    'endDateOfficialTextId',
+    'relationTypeId',
+    'resourceId',
     'startDate',
-    'endDate',
+    'startDateOfficialTextId',
   ];
   const body = {};
   Object.keys(form).forEach((key) => {
@@ -223,6 +225,22 @@ export default function RelationForm({
           createdAt={data.createdAt}
         />
         <Row>
+          <Col n="12">
+            <RadioGroup isInline>
+              <Radio
+                label="Actif"
+                value
+                checked={form?.active}
+                onChange={() => updateForm({ active: true })}
+              />
+              <Radio
+                label="Inactif"
+                value={false}
+                checked={!form?.active}
+                onChange={() => updateForm({ active: false })}
+              />
+            </RadioGroup>
+          </Col>
           {inverse ? (
             <Col n="12" className="fr-pb-2w">
               <SearchBar
@@ -313,6 +331,11 @@ export default function RelationForm({
               onDateChange={(v) => updateForm({ endDate: v })}
             />
           </Col>
+          <Checkbox
+            label="Date de fin inconnue mais passée"
+            onChange={(e) => updateForm({ active: !e.target.checked })}
+            checked={form.active === false}
+          />
           <Col n="12" className="fr-pb-2w">
             <SearchBar
               buttonLabel="Rechercher"
