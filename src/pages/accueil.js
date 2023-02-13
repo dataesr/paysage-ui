7849fc0b6b5f38a9dbb8ba86e4d49961f5185691
Mihoub Debache <debache.mihoub@gmail.com>
2023-02-13 17:32:21 +0100

    fix(pages-accueil): disable last actuality and last events on homepage

--- src/pages/accueil.js
+++ src/pages/accueil.js
@@ -256,7 +256,7 @@ export default function HomePage() {
               </>
             )}
           </Container>
-          <Container spacing="mb-8w">
+          {/* <Container spacing="mb-8w">
             <Row gutters>
               <Col n="6">
                 {(data?.data?.length > 0) && (
@@ -333,7 +333,7 @@ export default function HomePage() {
                 )}
               </Col>
             </Row>
-          </Container>
+          </Container> */}
         </>
       )}
     </Container>
