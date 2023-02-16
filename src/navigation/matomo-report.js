ed4039225741f3ee32c4cd6635fc36c03815b042
folland87 <frederic@olland.me>
2023-02-16 14:56:34 +0100

    add groups in matomo customDimentions

--- src/navigation/matomo-report.js
+++ src/navigation/matomo-report.js
@@ -2,12 +2,18 @@ import { useEffect } from 'react';
 
 import { useMatomo } from '@jonkoops/matomo-tracker-react';
 import { useLocation } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 export default function MatomoReport() {
+  const { viewer } = useAuth();
   const { trackPageView } = useMatomo();
   const { pathname } = useLocation();
 
   useEffect(() => {
-    trackPageView();
-  }, [trackPageView, pathname]);
+    trackPageView({
+      customDimensions: [
+        { id: 1, value: viewer?.groups?.map((group) => group.acronym).join(';') },
+      ],
+    });
+  }, [trackPageView, pathname, viewer]);
 }
