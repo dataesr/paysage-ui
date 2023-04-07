0f11b595f3ecdd38d4143e8666da52499062883e
folland87 <frederic@olland.me>
2023-04-07 14:33:26 +0200

    fix(gouvernance): Delete Référent MESR from person page

--- src/pages/personnes/[id]/mandats.js
+++ src/pages/personnes/[id]/mandats.js
@@ -1,28 +1,17 @@
 import { RelationsByTag } from '../../../components/blocs/relations';
 import MandateForm from '../../../components/forms/mandate';
-import { GOUVERNANCE, STRUCTURE_REFERENT_MESR } from '../../../utils/relations-tags';
+import { GOUVERNANCE } from '../../../utils/relations-tags';
 
 export default function PersonMandats() {
   return (
-    <>
-      <RelationsByTag
-        blocName="Mandats"
-        tag={GOUVERNANCE}
-        resourceType="structures"
-        relatedObjectTypes={['persons']}
-        Form={MandateForm}
-        inverse
-        sort="relationType.priority"
-      />
-      <RelationsByTag
-        tag={STRUCTURE_REFERENT_MESR}
-        blocName="Référent MESR"
-        resourceType="structures"
-        relatedObjectTypes={['persons']}
-        Form={MandateForm}
-        inverse
-        noRelationType
-      />
-    </>
+    <RelationsByTag
+      blocName="Mandats"
+      tag={GOUVERNANCE}
+      resourceType="structures"
+      relatedObjectTypes={['persons']}
+      Form={MandateForm}
+      inverse
+      sort="relationType.priority"
+    />
   );
 }
