cff07c56d67792b10d80c3a9e80104970a706264
Mihoub Debache <debache.mihoub@gmail.com>
2023-04-12 09:51:57 +0200

    typo

--- src/pages/projet-et-equipe.js
+++ src/pages/projet-et-equipe.js
@@ -61,14 +61,14 @@ export default function ProjetEtEquipe() {
               Mise en production de Paysage V1 (5 000 structures décrites)
             </TimelineItem>
             <TimelineItem date="2022-12" approximate>
-              Mise en production de Paysage V2. Ergonomie repensée et conforme au système de design de l’état, nouvelle architecture technique (10 000 structures décrites)
+              Mise en production de Paysage V2. Ergonomie repensée et conforme au système de design de l’État, nouvelle architecture technique (10 000 structures décrites)
             </TimelineItem>
           </Timeline>
         </Col>
         <Col n="12 md-8" spacing="pl-md-8w">
           <Title as="h2" look="h3">A l'origine du projet</Title>
           <Text>
-            Les différents services du ministère de l'Enseignement supérieur et de la Recherche (MESR) gèrent ou suivent de nombreux établissements publics et privés, partenaires, dispositifs ou réseaux, plus ou moins imbriqués les uns dans les autres. Toutes ces structures constituent le paysage de l'enseignement supérieur, de la recherche et de l'innovation. Ce paysage est en constant mouvement. L'ordonnance n° 2018-1131 relative à l'expérimentation de nouvelles formes de rapprochement, de regroupement ou de fusion des établissements d'enseignement supérieur et de recherche offre par exemple de nouvelles possibilités qui ont fortement modifié ce paysage.
+            Le MESR gère ou suit de nombreux établissements publics et privés, partenaires, dispositifs ou réseaux, plus ou moins imbriqués les uns dans les autres. Toutes ces structures constituent le paysage de l'enseignement supérieur, de la recherche et de l'innovation. Ce paysage est en constant mouvement. L'ordonnance n° 2018-1131 relative à l'expérimentation de nouvelles formes de rapprochement, de regroupement ou de fusion des établissements d'enseignement supérieur et de recherche offre par exemple de nouvelles possibilités qui ont fortement modifié ce paysage.
           </Text>
           <Text>
             Jusqu'à présent, faute d'outil commun, le suivi de la mise en œuvre d'une nouvelle réforme se faisait indépendamment dans chaque service interne ou externe au MESR. L'information est ainsi éparpillée, et pas forcément à jour partout. De même, aucun outil ne permettait jusqu'alors d'avoir une vue consolidée de ce paysage, de voir comment ce paysage évolue ou va évoluer puisque chaque dispositif avait son propre outil de suivi.
@@ -137,7 +137,7 @@ export default function ProjetEtEquipe() {
             <br />
             Alexandra Bounyavath a construit une communauté, formé et informé sans relâche les utilisateurs sur le projet et identifié pour l’équipe de conception les besoins des utilisateurs. Pauline Gaudet-Chardonnet a été à l’origine d’une première esquisse de design.
             <br />
-            Anne L’Hôte, Frédéric Olland et Jeremy Peglion ont imaginé une architecture technique à l’état de l’art. Associés à Mihoub Debache, ils ont développé l’interface de Paysage. Alexandra Bounyavath, Yann Caradec, Julia Grandhay, Isabelle Paulin, Mialy Rakondrazaka et Emmanuel Weisenburger ont travaillé à l’enrichissement et à la fiabilisation des données de Paysage.
+            Anne L’Hôte, Frédéric Olland et Jérémy Peglion ont imaginé une architecture technique à l’état de l’art. Associés à Mihoub Debache, ils ont développé l’interface de Paysage. Alexandra Bounyavath, Yann Caradec, Julia Grandhay, Isabelle Paulin, Mialy Rakondrazaka et Emmanuel Weisenburger ont travaillé à l’enrichissement et à la fiabilisation des données de Paysage.
           </Text>
           <Text>
             Paysage s'appuie également sur de nombreux contributeurs de la Direction générale de l'enseignement supérieur et de l'insertion professionnelle (DGESIP) et de la Direction générale de la recherche et de l'innovation (DGRI). Un grand merci à eux.
