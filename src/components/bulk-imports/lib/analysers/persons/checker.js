c95c8bf78e23c64e3cd0709b09bfee81a1829b88
Mihoub Debache <debache.mihoub@gmail.com>
2023-04-19 13:35:58 +0200

    fix(bulk-import): delete categories for persons and add researchgate to mapping

--- src/components/bulk-imports/lib/analysers/persons/checker.js
+++ src/components/bulk-imports/lib/analysers/persons/checker.js
@@ -47,40 +47,18 @@ async function idChecker(keyName, keyValue) {
   return [];
 }
 
-async function categoriesChecker({ categories }) {
-  if (!categories || categories.length === 0) return [{ message: 'Vous devez renseigner au moins une catégorie' }];
-  const categoriesWarning = [];
-  const wrongFormattedCategories = categories.filter((category) => category?.length !== 5);
-  if (wrongFormattedCategories.length > 0) {
-    const wrongFormattedCategoryLabels = wrongFormattedCategories.join('; ');
-    categoriesWarning.push({ message: `Les catégories suivantes ne sont pas correctement renseignées : ${wrongFormattedCategoryLabels}` });
-    return categoriesWarning;
-  }
-  if (categories) {
-    const requests = categories.map(async (category) => api.get(`/autocomplete?types=categories&query=${category}`));
-    const apiData = await Promise.all(requests);
-    const apiCategories = apiData?.map((el) => el.data.data?.[0]?.id);
-    categories.forEach((category) => {
-      if (!apiCategories.includes(category)) {
-        categoriesWarning.push({ message: `La catégorie ${category} n'existe pas` });
-      }
-    });
-  }
-  return categoriesWarning;
-}
-
 export default async function checker(docs, index) {
   try {
     const doc = docs[index];
     const nameDuplicateWarnings = await nameChecker(doc);
+    // const idDuplicateWarnings = await idChecker(['orcid', 'wikidata', 'idref'], doc);
     const orcidDuplicate = await idChecker('orcid', doc.orcid);
     const wikidataDuplicate = await idChecker('wikidata', doc.wikidata);
     const idrefDuplicate = await idChecker('idref', doc.idref);
     const websiteChecked = await websiteChecker(doc);
-    const categoriesChecked = await categoriesChecker(doc);
     const requiredErrors = requiredChecker(doc);
     const warning = [...nameDuplicateWarnings, ...orcidDuplicate, ...wikidataDuplicate, ...idrefDuplicate, ...websiteChecked];
-    const error = [...requiredErrors, ...categoriesChecked];
+    const error = [...requiredErrors];
     let status = 'success';
     if (warning.length) { status = 'warning'; }
     if (error.length) { status = 'error'; }
