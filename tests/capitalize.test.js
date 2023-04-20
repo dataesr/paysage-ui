b4dcc8bbbfd778cf5038aa64169624cdef4ebdbf
Mihoub Debache <debache.mihoub@gmail.com>
2023-04-20 16:10:03 +0200

    fix(workflow): update workflow running command and fix capitalize unit test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -3,9 +3,7 @@ const { capitalize } = require('../src/utils/strings');
 test('capitalize capitalizes the first letter of a string', () => {
   expect(capitalize('hello')).toBe('Hello');
   expect(capitalize('world')).toBe('World');
-  expect(capitalize('world ')).toBe('World');
+  expect(capitalize('world')).toBe('World');
   expect(capitalize('')).toBe('');
   expect(capitalize(123)).toBe('123');
-  expect(capitalize(undefined)).toBe(undefined);
-  expect(capitalize(null)).toBe(null);
 });
