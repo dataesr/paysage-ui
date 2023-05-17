2ca5e3de68ff30c77b90b55b0101e2561636a17f
folland87 <frederic@olland.me>
2023-05-17 15:56:22 +0200

    cgu without login

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -45,48 +45,50 @@ export default function Footer({ switchTheme }) {
           <Link target="_blank" href="https://data.gouv.fr">data.gouv.fr</Link>
         </FooterBodyItem>
       </FooterBody>
-      {viewer?.id && (
-        <FooterBottom>
-          <FooterLink asLink={<RouterLink to="/aide" />}>
-            Aide
-          </FooterLink>
-          <FooterLink asLink={<RouterLink to="/projet-et-equipe" />}>
-            L'équipe et son projet
-          </FooterLink>
-          <FooterLink asLink={<RouterLink to="/nous-contacter" />}>
-            Nous contacter
-          </FooterLink>
-          <FooterLink asLink={<RouterLink to="/mentions-legales" />}>
-            Mentions légales
-          </FooterLink>
-          <FooterLink asLink={<RouterLink to="/cgu" />}>
-            Conditions générales d'utilisation
-          </FooterLink>
-          <FooterLink asLink={<Link href="https://github.com/orgs/dataesr/repositories?q=paysage&type=&language=&sort=" target="_blank" />}>
-            Github
-          </FooterLink>
-          <FooterLink target="_blank" href={`https://github.com/dataesr/paysage-ui/releases/tag/v${process.env.REACT_APP_VERSION}`}>
-            {`Version de l'application v${process.env.REACT_APP_VERSION}`}
-          </FooterLink>
-          <FooterLink>
-            <button
-              onClick={() => setIsOpen(true)}
-              type="button"
-              className="fr-footer__bottom-link fr-fi-theme-fill fr-link--icon-left"
-              aria-controls="fr-theme-modal"
-              data-fr-opened={isOpen}
-            >
-              Paramètres d'affichage
-            </button>
-          </FooterLink>
-          <FooterCopy to="/">
-            <Text size="sm">
-              Sauf mention contraire, tous les contenus de ce site sont sous
-              <Link target="_blank" href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"> licence étalab2.0</Link>
-            </Text>
-          </FooterCopy>
-        </FooterBottom>
-      )}
+      <FooterBottom>
+        <FooterLink asLink={<RouterLink to="/mentions-legales" />}>
+          Mentions légales
+        </FooterLink>
+        <FooterLink asLink={<RouterLink to="/cgu" />}>
+          Conditions générales d'utilisation
+        </FooterLink>
+        {viewer?.id && (
+          <>
+            <FooterLink asLink={<RouterLink to="/aide" />}>
+              Aide
+            </FooterLink>
+            <FooterLink asLink={<RouterLink to="/projet-et-equipe" />}>
+              L'équipe et son projet
+            </FooterLink>
+            <FooterLink asLink={<RouterLink to="/nous-contacter" />}>
+              Nous contacter
+            </FooterLink>
+            <FooterLink asLink={<Link href="https://github.com/orgs/dataesr/repositories?q=paysage&type=&language=&sort=" target="_blank" />}>
+              Github
+            </FooterLink>
+            <FooterLink target="_blank" href={`https://github.com/dataesr/paysage-ui/releases/tag/v${process.env.REACT_APP_VERSION}`}>
+              {`Version de l'application v${process.env.REACT_APP_VERSION}`}
+            </FooterLink>
+          </>
+        )}
+        <FooterLink>
+          <button
+            onClick={() => setIsOpen(true)}
+            type="button"
+            className="fr-footer__bottom-link fr-fi-theme-fill fr-link--icon-left"
+            aria-controls="fr-theme-modal"
+            data-fr-opened={isOpen}
+          >
+            Paramètres d'affichage
+          </button>
+        </FooterLink>
+        <FooterCopy to="/">
+          <Text size="sm">
+            Sauf mention contraire, tous les contenus de ce site sont sous
+            <Link target="_blank" href="https://github.com/etalab/licence-ouverte/blob/master/LO.md"> licence étalab2.0</Link>
+          </Text>
+        </FooterCopy>
+      </FooterBottom>
     </FooterWrapper>
   );
 }
