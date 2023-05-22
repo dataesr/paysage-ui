2602453021c0620ddc8d9bf4623a1a739ec41f10
Mihoub Debache <debache.mihoub@gmail.com>
2023-05-22 15:09:26 +0200

    typo

--- src/pages/creer-un-compte.js
+++ src/pages/creer-un-compte.js
@@ -141,7 +141,7 @@ export default function SignUp() {
                               <br />
                               <PasswordHint display={(passwordValidation.length === true) ? 'success' : passwordErrorDisplay} hint="8 caractères minimum" />
                               <PasswordHint display={(passwordValidation.uppercase === true) ? 'success' : passwordErrorDisplay} hint="1 majuscule minimum" />
-                              <PasswordHint display={(passwordValidation.lowercase === true) ? 'success' : passwordErrorDisplay} hint="1 miniscule minimum" />
+                              <PasswordHint display={(passwordValidation.lowercase === true) ? 'success' : passwordErrorDisplay} hint="1 minuscule minimum" />
                               <PasswordHint
                                 display={(passwordValidation.special === true) ? 'success' : passwordErrorDisplay}
                                 hint={'1 caractère spécial minimum parmi !"#$%&\'()`*+,-./:;<=>?@[]^_{|}~'}
