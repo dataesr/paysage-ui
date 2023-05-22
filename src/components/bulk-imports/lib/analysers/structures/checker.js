497c9a9c3b781c69e52f0201d6189ad0b01b3e4d
Mihoub Debache <debache.mihoub@gmail.com>
2023-05-22 12:53:43 +0200

    fix(bulk-import structures): add checker for duplicate in import file

--- src/components/bulk-imports/lib/analysers/structures/checker.js
+++ src/components/bulk-imports/lib/analysers/structures/checker.js
@@ -67,6 +67,27 @@ function websiteChecker({ websiteFr, websiteEn }) {
   return [];
 }
 
+function duplicateInImportFile(docs) {
+  const allValues = {};
+  const errors = [];
+
+  for (let i = 0; i < docs.length; i += 1) {
+    const el = docs[i];
+    const entries = Object.entries(el);
+    for (let j = 0; j < entries.length; j += 1) {
+      const [key, value] = entries[j];
+      if (key !== 'structureStatus' && key !== 'legalCategory' && key !== 'cityId'
+       && key !== 'iso3' && key !== 'country' && key !== 'category' && value && allValues[key] && allValues[key].includes(value)) {
+        errors.push({ message: `La valeur "${value}" pour la clé "${key}" existe déjà dans votre fichier d'import` });
+      } else {
+        allValues[key] = allValues[key] || [];
+        allValues[key].push(value);
+      }
+    }
+  }
+  return errors;
+}
+
 async function categoriesChecker({ categories }) {
   if (!categories || categories.length === 0) return [{ message: 'Vous devez renseigner au moins une catégorie' }];
   const categoriesWarning = [];
@@ -123,6 +144,7 @@ export default async function checker(docs, index) {
     const doc = docs[index];
     const categoriesErrors = await categoriesChecker(doc);
     const nameDuplicateWarnings = await nameChecker(doc);
+    const isDuplicatedInImportFile = await duplicateInImportFile(docs);
     const requiredErrors = requiredChecker(doc);
     const edDuplicate = await duplicateIdChecker('ed', doc.ed);
     const idrefDuplicate = await duplicateIdChecker('idref', doc.idref);
@@ -140,7 +162,8 @@ export default async function checker(docs, index) {
     const wikidataFormat = await idFormatChecker('wikidata', doc.wikidata);
     const legalCategoryCheck = await legalCategoriesChecker(doc);
     const websiteChecked = await websiteChecker(doc);
-    const warning = [...idrefDuplicate, ...siretDuplicate, ...edDuplicate, ...rnsrDuplicate, ...uaiDuplicate, ...rorDuplicate, ...wikidataDuplicate, ...nameDuplicateWarnings, ...websiteChecked];
+    const warning = [...idrefDuplicate, ...isDuplicatedInImportFile, ...siretDuplicate,
+      ...edDuplicate, ...rnsrDuplicate, ...uaiDuplicate, ...rorDuplicate, ...wikidataDuplicate, ...nameDuplicateWarnings, ...websiteChecked];
     const error = [...requiredErrors, ...legalCategoryCheck, ...categoriesErrors, ...edFormat, ...idrefFormat, ...siretFormat, ...rnsrFormat, ...rorFormat, ...uaiFormat, ...wikidataFormat];
     let status = 'success';
     if (warning.length) { status = 'warning'; }
