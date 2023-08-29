fe16326fddf93a4f33f7f8f2faaab12ef7a7ca19
Mihoub Debache <debache.mihoub@gmail.com>
2023-08-29 14:57:28 +0200

    fix(analysers): add type

--- src/components/bulk-imports/lib/analysers/persons/index.js
+++ src/components/bulk-imports/lib/analysers/persons/index.js
@@ -1,6 +1,6 @@
 import { parseTSV, zip } from '../utils';
 import checker from './checker';
-import headersMapping from './headers-mapping';
+import { personsHeadersMapping } from './headers-mapping';
 
 const genderMapping = {
   F: 'Femme',
@@ -15,12 +15,13 @@ const personParsingFunctions = {
 
 export default async function parsePersonTSV(inputString) {
   const { headers, rows } = parseTSV(inputString);
-  const orderedApiHeaders = headers.map((header) => headersMapping[header]);
+  const type = 'persons';
+  const orderedApiHeaders = headers.map((header) => personsHeadersMapping[header]);
   const bodyList = rows.map((row) => zip(orderedApiHeaders, row, personParsingFunctions));
   const result = await Promise.all(bodyList.map(async (body, index) => {
     const { warning, error, status } = await checker(bodyList, index);
     const displayName = `${body.firstName || ''} ${body.lastName || ''}`.trim();
-    return { index: index + 2, body, displayName, warning, error, status };
+    return { index: index + 2, body, displayName, type, warning, error, status };
   }));
   return result;
 }
