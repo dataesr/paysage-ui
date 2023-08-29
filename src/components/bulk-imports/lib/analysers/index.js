c299efc41a4904f6acc314605a2499dce2582590
Mihoub Debache <debache.mihoub@gmail.com>
2023-08-29 15:00:03 +0200

    fix(importers): add gouvernance to analysis route

--- src/components/bulk-imports/lib/analysers/index.js
+++ src/components/bulk-imports/lib/analysers/index.js
@@ -1,16 +1,27 @@
 import parsePersonTSV from './persons';
 import parseStructureTSV from './structures';
+import parseRelationsTSV from './relations';
 
 export default async function analyse(str, type) {
   let analyses;
   switch (type) {
   case 'structures':
     analyses = await parseStructureTSV(str);
-    if (analyses.length > 0) { return analyses; }
+    if (analyses.length > 0) {
+      return analyses;
+    }
     throw new Error('File Error');
   case 'personnes':
     analyses = await parsePersonTSV(str);
-    if (analyses.length > 0) { return analyses; }
+    if (analyses.length > 0) {
+      return analyses;
+    }
+    throw new Error('File Error');
+  case 'gouvernance':
+    analyses = await parseRelationsTSV(str);
+    if (analyses.length > 0) {
+      return analyses;
+    }
     throw new Error('File Error');
   default:
     throw new Error('Not implemented');
