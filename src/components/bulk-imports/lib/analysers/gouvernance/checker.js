8466f10bde58d2651791f2f4d61e84dc581ea58c
Mihoub Debache <debache.mihoub@gmail.com>
2023-09-04 10:37:17 +0200

    fix(bulk-import): update checker for governance

--- src/components/bulk-imports/lib/analysers/gouvernance/checker.js
+++ src/components/bulk-imports/lib/analysers/gouvernance/checker.js
@@ -23,14 +23,26 @@ async function personChecker(personId) {
   }
   return personWarning;
 }
-
 async function relationTypeChecker(relationTypeId) {
   const personWarning = [];
   if (relationTypeId) {
-    const response = await api.get(`https://api.paysage.staging.dataesr.ovh/relation-types/${relationTypeId}`);
-    const apiCategory = response.data.data?.[0]?.id;
-    if (!apiCategory) {
-      personWarning.push({ message: `Le code de la fonction/responsabilité ${relationTypeId} n'existe pas` });
+    try {
+      const response = await api.get(`/relation-types/${relationTypeId}`);
+      if (!response.ok) {
+        throw new Error(`La requête a échoué avec le code d'erreur : ${response.status}`);
+      }
+
+      if (!response.bodyUsed) {
+        const responseData = await response.json();
+        const apiCategory = responseData.data?.[0]?.id;
+
+        if (!apiCategory) {
+          personWarning.push({ message: `Le code de la fonction/responsabilité ${relationTypeId} n'existe pas` });
+        }
+      }
+    } catch (error) {
+      // console.log(error);
+      personWarning.push({ message: 'Une erreur s\'est produite lors de la requête à l\'API' });
     }
   }
   return personWarning;
@@ -38,18 +50,10 @@ async function relationTypeChecker(relationTypeId) {
 
 async function mandatePositionChecker(mandatePosition) {
   const mandatePositionWarning = [];
-  if (mandatePosition !== 1 || mandatePosition !== 2 || mandatePosition !== 3 || mandatePosition.length !== 0) {
-    mandatePositionWarning.push({ message: 'Veuillez saisir 1,2,3 ou laisse un vide' });
+  if (mandatePosition !== '1' && mandatePosition !== '2' && mandatePosition !== '3+' && mandatePosition.length !== 0) {
+    mandatePositionWarning.push({ message: 'Veuillez saisir 1, 2, 3 ou laisser vide' });
   }
   return mandatePositionWarning;
-}
-
-async function mandateTemporaryChecker(mandateTemporary) {
-  const mandateTemporaryWarning = [];
-  if (mandateTemporary !== 'O' || mandateTemporary !== 'N') {
-    mandateTemporaryWarning.push({ message: 'Veuillez saisir O ou N' });
-  }
-  return mandateTemporaryWarning;
 }
 
 async function phoneNumberChecker(mandatePhonenumber) {
@@ -63,20 +67,19 @@ async function phoneNumberChecker(mandatePhonenumber) {
 export default async function checker(docs, index) {
   try {
     const doc = docs[index];
-
     const prizeCheck = await structureChecker(doc?.resourceId);
     const personCheck = await personChecker(doc?.relatedObjectId);
     const relationTypeCheck = await relationTypeChecker(doc?.relationTypeId);
     const mandatePositionCheck = await mandatePositionChecker(doc?.mandatePosition);
-    const mandateTemporaryCheck = await mandateTemporaryChecker(doc?.mandateTemporary);
     const phoneNumberCheck = await phoneNumberChecker(doc?.mandatePhonenumber);
-    const warning = [...mandatePositionCheck, ...phoneNumberCheck, ...mandateTemporaryCheck];
+    const warning = [...mandatePositionCheck, ...phoneNumberCheck];
     const error = [...prizeCheck, ...personCheck, ...relationTypeCheck];
     let status = 'success';
     if (warning.length) { status = 'warning'; }
     if (error.length) { status = 'error'; }
     return { warning, error, status };
   } catch (e) {
+    // console.log(e);
     return { error: [{ message: "Une erreur s'est produite lors de la vérification, vérifiez la ligne" }], status: 'error' };
   }
 }
