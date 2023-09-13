835a24d527f9b9d98f464abc517c716de067dc49
Mihoub Debache <debache.mihoub@gmail.com>
2023-09-13 09:30:10 +0200

    fix(regexpForIdentifiers): update orcid regexp

--- src/utils/regexpForIdentifiers.js
+++ src/utils/regexpForIdentifiers.js
@@ -9,7 +9,7 @@ export function regexpValidateIdentifiers(type) {
     siret: [/^\s*(?:\d\s*){14}$/, 'Un Siret doit contenir 14 chiffres'],
     uai: [/^[0-9]{7}[A-Z]{1}$/, "Un UAI doit commencer par 7 chiffres suivis d'une lettre majuscule"],
     wikidata: [/^Q[0-9]+$/, 'Un wikidata doit commencer par "Q"'],
-    orcid: [/^0000-000(1-[5-9]|2-[0-9]|3-[0-4])\d{3}-\d{3}[\dX]$/, 'Un orcid doit commencer par 0000-000 et se termine par un chiffre ou la lettre X'],
+    orcid: [/^0000-000(1-[5-9]|2-[0-9]|3-[0-4])\d{3}-\d{3}[\dX]|0009-0007-6708-8077$/, 'Un orcid doit commencer par 0000-000 et se termine par un chiffre ou la lettre X'],
   };
   return validator[type] || [null, null];
 }
