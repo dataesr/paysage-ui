099c0112b1c96725089cc0433bab730197d31bd5
Mihoub Debache <debache.mihoub@gmail.com>
2023-10-12 16:24:56 +0200

    fix(identifiers): orcid regexp update

--- src/utils/regexpForIdentifiers.js
+++ src/utils/regexpForIdentifiers.js
@@ -11,8 +11,7 @@ export function regexpValidateIdentifiers(type) {
     'piaweb-project': [/^\d{1,10}$/, 'Un PIA ne contient que des chiffres'],
     uai: [/^[0-9]{7}[A-Z]{1}$/, "Un UAI doit commencer par 7 chiffres suivis d'une lettre majuscule"],
     wikidata: [/^Q[0-9]+$/, 'Un wikidata doit commencer par "Q"'],
-
-    orcid: [/^0000-000(1-[5-9]|2-[0-9]|3-[0-4])\d{3}-\d{3}[\dX]|0009-0007-6708-8077$/, 'Un orcid doit commencer par 0000-000 et se termine par un chiffre ou la lettre X'],
+    orcid: [/^\d{4}-\d{4}-\d{4}-\d{3}[0-9X]$/, 'Un orcid doit contenir 4 fois 4 chiffres et se termine par un chiffre ou un "X"'],
   };
   return validator[type] || [null, null];
 }
