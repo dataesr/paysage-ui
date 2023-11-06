5ddde17351260b45c42c9329eac821d0dded94f9
Mihoub Debache <debache.mihoub@gmail.com>
2023-11-06 10:12:03 +0100

    fix(timeline): display the month or the year only if approximate

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 import styles from './styles.module.scss';
 
-export function TimelineItem({ approximate, date, children }) {
+export function TimelineItem({ date, children }) {
+  const isApproximate = date.length <= 7;
   const options = {
     year: 'numeric',
     month: 'short',
@@ -12,8 +13,8 @@ export function TimelineItem({ approximate, date, children }) {
   return (
     <div className={`${styles['timeline-item']}`}>
       <div className={styles['timeline-date']}>
-        {(approximate && day === '1') ? null : <span className="fr-h3 fr-mb-0">{day}</span>}
-        {(approximate && month === '1') ? null : <span className="fr-text--sm fr-mb-0">{month}</span>}
+        {(isApproximate && day === '1') ? null : <span className="fr-h3 fr-mb-0">{day}</span>}
+        {(isApproximate && month === '1') ? null : <span className="fr-text--sm fr-mb-0">{month}</span>}
         <span className="fr-text--sm fr-mb-0">{year}</span>
       </div>
       <div className={styles['timeline-content']}>
@@ -23,12 +24,7 @@ export function TimelineItem({ approximate, date, children }) {
   );
 }
 
-TimelineItem.defaultProps = {
-  approximate: false,
-};
-
 TimelineItem.propTypes = {
-  approximate: PropTypes.bool,
   date: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 };
