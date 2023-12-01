b37bb322627ce4b38b139651fd30696ded2cf808
Mihoub Debache <debache.mihoub@gmail.com>
2023-12-01 14:41:05 +0100

    fix(journal-modifcation): use search params for pagination

--- src/components/blocs/modification-journal/index.js
+++ src/components/blocs/modification-journal/index.js
@@ -1,44 +1,41 @@
 import React, { useState } from 'react';
 import { Accordion, AccordionItem, Col, Pagination, Row } from '@dataesr/react-dsfr';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import useFetch from '../../../hooks/useFetch';
 import { Bloc, BlocContent, BlocTitle } from '../../bloc';
 import ModificationDetails from './components/modification-details';
 import ModificationTitle from './components/modification-title';
 
 const LAST_DAYS = 15;
-const pageSize = 30;
-
 const DATE = new Date(Date.now() - LAST_DAYS * 24 * 60 * 60 * 1000).toISOString();
 
 export default function ModificationJournal() {
   const [openAccordions, setOpenAccordions] = useState([]);
-  const [page, setPage] = useState(1);
-
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = searchParams.get('page') || 1;
+  const limit = 50;
+  const skip = (page - 1) * 50;
   const { id: resourceId } = useParams();
   const url = resourceId
-    ? `/journal?filters[resourceId]=${resourceId}&filters[createdAt][$gte]=${DATE}&sort=-createdAt&limit=200&filters[resourceType][$ne]=admin&filters[resourceType][$ne]=groups`
-    : `/journal?filters[createdAt][$gte]=${DATE}&sort=-createdAt&limit=200&filters[resourceType][$ne]=admin`;
+    ? `/journal?filters[resourceId]=${resourceId}&filters[createdAt][$gte]=${DATE}&sort=-createdAt&skip=${skip}&limit=${limit}&filters[resourceType][$ne]=admin&filters[resourceType][$ne]=groups`
+    : `/journal?filters[createdAt][$gte]=${DATE}&sort=-createdAt&skip=${skip}&limit=${limit}&filters[resourceType][$ne]=admin`;
 
   const { data, error, isLoading } = useFetch(url);
-
-  const handlePaginationClick = (newPage) => {
-    setPage(newPage);
-  };
-
-  const startIndex = (page - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
+  if (!data?.totalCount) {
+    return null;
+  }
 
   return (
+
     <Bloc isLoading={isLoading} error={error} data={data}>
       <BlocTitle as="h1" look="h3">Journal des modifications</BlocTitle>
       <BlocContent>
         <Row gutters>
           <Col n="12">
             <Accordion keepOpen>
-              {data?.data?.length && data.data.slice(startIndex, endIndex).map((event, i) => (
-                <AccordionItem onClick={() => setOpenAccordions((prev) => [...prev, i + startIndex])} key={event.createdAt} title={<ModificationTitle data={event} />}>
-                  {openAccordions.includes(i + startIndex) && <ModificationDetails data={event} />}
+              {data?.data?.length && data.data.map((event, i) => (
+                <AccordionItem onClick={() => setOpenAccordions((prev) => [...prev, i])} key={event.createdAt} title={<ModificationTitle data={event} />}>
+                  {openAccordions.includes(i) && <ModificationDetails data={event} />}
                 </AccordionItem>
               ))}
             </Accordion>
@@ -46,10 +43,10 @@ export default function ModificationJournal() {
         </Row>
         <Row className="flex--space-around fr-pt-3w">
           <Pagination
+            onClick={(newPage) => setSearchParams({ page: newPage })}
             surrendingPages={2}
-            currentPage={page}
-            pageCount={data ? Math.ceil(data.totalCount / pageSize) : 0}
-            onClick={handlePaginationClick}
+            currentPage={Number(page)}
+            pageCount={data?.totalCount ? Math.ceil(data.totalCount / limit) : 0}
           />
         </Row>
       </BlocContent>
