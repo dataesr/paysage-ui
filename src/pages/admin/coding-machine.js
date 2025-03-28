60b25fb628c495250c7255979f5ea969ab8c68d4
Mihoub <debache.mihoub@gmail.com>
2025-03-28 08:30:26 +0100

    fix(coding-machine): now only a list of id can be used

--- src/pages/admin/coding-machine.js
+++ src/pages/admin/coding-machine.js
@@ -93,15 +93,52 @@ export default function CodingMachinePage() {
         <Col>
           <Title as="h2">Machine à coder</Title>
           <Text size="sm">Copiez et collez un tableau depuis Excel ou CSV pour trouver les identifiants Paysage correspondants.</Text>
+          <Text size="sm">
+            Les colonnes du tableau doivent contenir des noms de
+            {' '}
+            {getTypeLabel()}
+            .
+            La première colonne doit avoir pour nom "Name", et les suivantes les noms d'identifiants
+          </Text>
+          <Text size="sm">
+            Les colonnes du tableau peuvent contenir:
+            {' '}
 
+            <ul>
+              <li>Soit une première colonne "Name" suivie d'identifiants (format standard)</li>
+              <li>Soit directement des identifiants (SIRET, UAI, RNSR, etc.) sans colonne "Name"</li>
+            </ul>
+          </Text>
+
+          Exemple :
+          <Row gutters className="fr-mb-3w">
+            <Col n="6">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>SIRET</th>
+                  <th>UAI</th>
+                </tr>
+                <tr>
+                  <td>Structure 1</td>
+                  <td>12345678901234</td>
+                  <td>0123456A</td>
+                </tr>
+              </thead>
+            </Col>
+            <Col n="6">
+              <thead>
+                <tr>
+                  <th>Siret</th>
+                </tr>
+                <tr>
+                  <td>12345678901234</td>
+                </tr>
+              </thead>
+            </Col>
+          </Row>
           <div className="fr-mb-3w">
-            <Text size="sm">
-              Les colonnes du tableau doivent contenir des noms de
-              {' '}
-              {getTypeLabel()}
-              .
-              La première colonne doit avoir pour nom "Name", et les suivantes les noms d'identifiants
-            </Text>
+
             <Text bold className="fr-mb-1w">Type d'entités à rechercher :</Text>
             <RadioGroup
               legend=""
