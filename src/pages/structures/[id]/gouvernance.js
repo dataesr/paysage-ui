00b8dccbd3da4e1974b1ba32dedc81b3b711bcbe
Mihoub Debache <debache.mihoub@gmail.com>
2025-11-19 15:39:23 +0100

    fix(gouvernances): delete tab for simple view

--- src/pages/structures/[id]/gouvernance.js
+++ src/pages/structures/[id]/gouvernance.js
@@ -1,4 +1,4 @@
-import { Tab, Tabs, Col, Row } from '@dataesr/react-dsfr';
+import { Col, Row } from '@dataesr/react-dsfr';
 import { RelationsGouvernance } from '../../../components/blocs/relations';
 import MandateForm from '../../../components/forms/mandate';
 import { GOUVERNANCE } from '../../../utils/relations-tags';
@@ -7,35 +7,19 @@ import EmailsComponent from '../../../components/blocs/emails';
 export default function StructureGouvernancePage() {
   return (
     <Row gutters>
-      <Tabs>
-        <Tab
-          label="Gouvernance"
-          className="fr-card fr-card--xs fr-card--horizontal fr-card--grey fr-card--no-border"
-        >
-          <Row style={{ overflowY: 'scroll' }}>
-            <Col n="12">
-              <RelationsGouvernance
-                tag={GOUVERNANCE}
-                blocName="Gouvernance, administration et référents"
-                resourceType="structures"
-                relatedObjectTypes={['persons']}
-                Form={MandateForm}
-                sort="relationType.priority"
-              />
-            </Col>
-          </Row>
-        </Tab>
-        <Tab
-          label="Contacts"
-          className="fr-card fr-card--xs fr-card--horizontal fr-card--grey fr-card--no-border"
-        >
-          <Row style={{ overflowY: 'scroll' }}>
-            <Col n="12">
-              <EmailsComponent />
-            </Col>
-          </Row>
-        </Tab>
-      </Tabs>
+      <Col n="12">
+        <EmailsComponent />
+      </Col>
+      <Col n="12">
+        <RelationsGouvernance
+          tag={GOUVERNANCE}
+          blocName="Gouvernance, administration et référents"
+          resourceType="structures"
+          relatedObjectTypes={['persons']}
+          Form={MandateForm}
+          sort="relationType.priority"
+        />
+      </Col>
     </Row>
   );
 }
