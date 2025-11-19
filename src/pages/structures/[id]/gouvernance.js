90c3e292dc975c20945bc9da811eac62390cd13c
Mihoub Debache <debache.mihoub@gmail.com>
2025-11-19 15:08:11 +0100

    fix(gouvernance): add tabs for gouvernance & contacts

--- src/pages/structures/[id]/gouvernance.js
+++ src/pages/structures/[id]/gouvernance.js
@@ -1,16 +1,41 @@
+import { Tab, Tabs, Col, Row } from '@dataesr/react-dsfr';
 import { RelationsGouvernance } from '../../../components/blocs/relations';
 import MandateForm from '../../../components/forms/mandate';
 import { GOUVERNANCE } from '../../../utils/relations-tags';
+import EmailsComponent from '../../../components/blocs/emails';
 
 export default function StructureGouvernancePage() {
   return (
-    <RelationsGouvernance
-      tag={GOUVERNANCE}
-      blocName="Gouvernance, administration et référents"
-      resourceType="structures"
-      relatedObjectTypes={['persons']}
-      Form={MandateForm}
-      sort="relationType.priority"
-    />
+    <Row gutters>
+      <Tabs>
+        <Tab
+          label="Gouvernance"
+          className="fr-card fr-card--xs fr-card--horizontal fr-card--grey fr-card--no-border"
+        >
+          <Row style={{ overflowY: 'scroll' }}>
+            <Col n="12">
+              <RelationsGouvernance
+                tag={GOUVERNANCE}
+                blocName="Gouvernance, administration et référents"
+                resourceType="structures"
+                relatedObjectTypes={['persons']}
+                Form={MandateForm}
+                sort="relationType.priority"
+              />
+            </Col>
+          </Row>
+        </Tab>
+        <Tab
+          label="Contacts"
+          className="fr-card fr-card--xs fr-card--horizontal fr-card--grey fr-card--no-border"
+        >
+          <Row style={{ overflowY: 'scroll' }}>
+            <Col n="12">
+              <EmailsComponent />
+            </Col>
+          </Row>
+        </Tab>
+      </Tabs>
+    </Row>
   );
 }
