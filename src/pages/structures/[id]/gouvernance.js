a19bfc756d69c6d5673800acf2752a4ee229b3e1
Mihoub Debache <debache.mihoub@gmail.com>
2025-11-20 11:27:36 +0100

    fix(gouvernance): redo tabs

--- src/pages/structures/[id]/gouvernance.js
+++ src/pages/structures/[id]/gouvernance.js
@@ -1,4 +1,4 @@
-import { Col, Row } from '@dataesr/react-dsfr';
+import { Col, Row, Tabs, Tab } from '@dataesr/react-dsfr';
 import { RelationsGouvernance } from '../../../components/blocs/relations';
 import MandateForm from '../../../components/forms/mandate';
 import { GOUVERNANCE } from '../../../utils/relations-tags';
@@ -7,19 +7,35 @@ import EmailsComponent from '../../../components/blocs/emails';
 export default function StructureGouvernancePage() {
   return (
     <Row gutters>
-      <Col n="12" className="fr-mt-2w">
-        <EmailsComponent />
-      </Col>
-      <Col n="12">
-        <RelationsGouvernance
-          tag={GOUVERNANCE}
-          blocName="Gouvernance, administration et référents"
-          resourceType="structures"
-          relatedObjectTypes={['persons']}
-          Form={MandateForm}
-          sort="relationType.priority"
-        />
-      </Col>
+      <Tabs>
+        <Tab
+          label="Gouvernance"
+          className="fr-card fr-card--xs fr-card--horizontal fr-card--grey fr-card--no-border"
+        >
+          <Row style={{ overflowY: 'scroll' }}>
+            <Col n="12">
+              <RelationsGouvernance
+                tag={GOUVERNANCE}
+                blocName="Gouvernance, administration et référents"
+                resourceType="structures"
+                relatedObjectTypes={['persons']}
+                Form={MandateForm}
+                sort="relationType.priority"
+              />
+            </Col>
+          </Row>
+        </Tab>
+        <Tab
+          label="Contacts"
+          className="fr-card fr-card--xs fr-card--horizontal fr-card--grey fr-card--no-border"
+        >
+          <Row style={{ overflowY: 'scroll' }}>
+            <Col n="12">
+              <EmailsComponent />
+            </Col>
+          </Row>
+        </Tab>
+      </Tabs>
     </Row>
   );
 }
