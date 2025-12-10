55aac18ea3da61c9f8ff794dcc33d8b72d34e8b8
Mihoub Debache <debache.mihoub@gmail.com>
2025-12-10 14:47:44 +0100

    fix(gouvernance): update gouvernance display, and sort document list

--- src/pages/structures/[id]/gouvernance.js
+++ src/pages/structures/[id]/gouvernance.js
@@ -1,7 +1,6 @@
 import { RelationsGouvernance } from '../../../components/blocs/relations';
 import MandateForm from '../../../components/forms/mandate';
 import { GOUVERNANCE } from '../../../utils/relations-tags';
-import EmailsComponent from '../../../components/blocs/emails';
 
 export default function StructureGouvernancePage() {
   return (
@@ -11,11 +10,6 @@ export default function StructureGouvernancePage() {
       relatedObjectTypes={['persons']}
       Form={MandateForm}
       sort="relationType.priority"
-      withTabs
-      ExtraTab={{
-        label: 'Boites email génériques',
-        component: <EmailsComponent />,
-      }}
     />
   );
 }
